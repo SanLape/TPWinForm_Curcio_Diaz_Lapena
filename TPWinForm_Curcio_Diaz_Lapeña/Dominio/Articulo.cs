c89ed9c1f6ca5247d74cacc420ac1d8fee8208c9
Santiago Lapeña <slapena88@gmail.com>
2023-04-29 12:01:37 +0200

    Creación del proyecto + Clases

---
+++ TPWinForm_Curcio_Diaz_Lapeña/Dominio/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        //Id,Codigo,Nombre,Descripcion,IdMarca,IdCategoria,ImagenUrl,Precio
+
+        public int ID { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenURL { get; set; }
+        public decimal precio { get; set; }
+        public int IDMARCAR { get; set; }
+        public int IDCATEGORIA { get; set; }
+    }
+}
