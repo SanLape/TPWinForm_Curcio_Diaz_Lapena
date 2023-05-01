602767d8052f50f4bb68b1d64d9d9e07ed4efe4e
Santiago Lapeña <slapena88@gmail.com>
2023-05-01 12:52:27 +0200

    Clase Negocio Marca

---
+++ TPWinForm_Curcio_Diaz_Lapeña/Negocio/MarcaNegocio.cs
@@ -0,0 +1,46 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class MarcaNegocio
+    {
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();  // LISTA DEL OBJETO
+            AccesoDatos datos = new AccesoDatos();          // SE CONFIGURA LA CONECCION (EN ACCESO DATOS)
+
+            try
+            {
+
+                datos.setConsulta("SELECT Id, Descripcion FROM MARCAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.ID = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Description"];
+
+                    lista.Add(aux);
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+    }
+}
