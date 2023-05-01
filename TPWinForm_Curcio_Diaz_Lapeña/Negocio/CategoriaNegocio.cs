349f38d0c6642539a33b72806e1f3304b61b2cc1
Santiago Lapeña <slapena88@gmail.com>
2023-05-01 12:17:48 +0200

    Agregado clase negocio Categoria

---
+++ TPWinForm_Curcio_Diaz_Lapeña/Negocio/CategoriaNegocio.cs
@@ -0,0 +1,45 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();  // LISTA DEL OBJETO
+            AccesoDatos datos = new AccesoDatos();          // SE CONFIGURA LA CONECCION (EN ACCESO DATOS)
+            
+            try
+            {
+
+                datos.setConsulta("SELECT Id, Descripcion FROM CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.ID = (int)datos.Lector["Id"];
+                    aux.Descripcion = (string)datos.Lector["Description"];
+
+                    lista.Add(aux);
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
